b7f5ba8b0c0e6e123a7d2248b23c8d8e76402002
Oussama Yaqdane <rakaoran@gmail.com>
2025-12-27 07:14:59 +0100

    auth testing completed

--- api/internal/handlers/auth.go
+++ api/internal/handlers/auth.go
@@ -10,7 +10,7 @@ import (
 var (
 	ErrorMissingTokenJson          = gin.H{"error": "missing-token"}
 	ErrorInvalidTokenJson          = gin.H{"error": "invalid-token"}
-	ErrorInvalidRequestFormatJson  = gin.H{"error": "wrong-request-format"}
+	ErrorInvalidRequestFormatJson  = gin.H{"error": "bad-request-format"}
 	ErrorInvalidCredentialsJson    = gin.H{"error": "invalid-credentials"}
 	ErrorUnknownErrorJson          = gin.H{"error": "unknown-error"}
 	ErrorUsernameAlreadyExistsJson = gin.H{"error": "username-already-exists"}
@@ -35,12 +35,12 @@ func NewAuthHandler(service AuthService) *AuthHandler {
 func (ah *AuthHandler) RequireAuthMiddleware(ctx *gin.Context) {
 	token, err := ctx.Cookie("token")
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusForbidden, ErrorMissingTokenJson)
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, ErrorMissingTokenJson)
 		return
 	}
 	username, err := ah.authService.VerifyToken(token)
 	if err != nil {
-		ctx.AbortWithStatusJSON(http.StatusForbidden, ErrorInvalidTokenJson)
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, ErrorInvalidTokenJson)
 		return
 	}
 
@@ -77,7 +77,7 @@ func (ah *AuthHandler) LoginHandler(ctx *gin.Context) {
 
 	ctx.SetCookie("token", token, 60*60*24*365*40, "/", "", true, true)
 	ctx.SetSameSite(http.SameSiteNoneMode)
-	ctx.Status(http.StatusOK)
+	ctx.JSON(http.StatusOK, gin.H{})
 }
 
 func (ah *AuthHandler) SignupHandler(ctx *gin.Context) {
@@ -111,7 +111,7 @@ func (ah *AuthHandler) SignupHandler(ctx *gin.Context) {
 
 	ctx.SetCookie("token", token, 60*60*24*365*40, "/", "", true, true)
 	ctx.SetSameSite(http.SameSiteNoneMode)
-	ctx.Status(http.StatusOK)
+	ctx.JSON(http.StatusCreated, gin.H{})
 }
 
 func (ah *AuthHandler) LogoutHandler(ctx *gin.Context) {
--- api/internal/handlers/auth_test.go
+++ api/internal/handlers/auth_test.go
@@ -46,10 +46,50 @@ func TestSignupHandler(t *testing.T) {
 		{
 			description:  "Username already exists",
 			signupFunc:   func(s1, s2 string) (string, error) { return "", auth.UsernameAlreadyExistsErr },
-			body:         `{"username":"haaha", "password": "iems"}`,
+			body:         `{}`,
 			expectedCode: http.StatusConflict,
 			expectedBody: `{"error": "username-already-exists"}`,
 			cookieIssued: false,
+		},
+		{
+			description:  "Invalid username format",
+			signupFunc:   func(s1, s2 string) (string, error) { return "", auth.InvalidUsernameFormatErr },
+			body:         `{}`,
+			expectedCode: http.StatusBadRequest,
+			expectedBody: `{"error": "invalid-username-format"}`,
+			cookieIssued: false,
+		},
+		{
+			description:  "weak password",
+			signupFunc:   func(s1, s2 string) (string, error) { return "", auth.WeakPasswordErr },
+			body:         `{}`,
+			expectedCode: http.StatusBadRequest,
+			expectedBody: `{"error": "weak-password"}`,
+			cookieIssued: false,
+		},
+		{
+			description:  "non json request",
+			signupFunc:   func(s1, s2 string) (string, error) { return "", nil },
+			body:         `{`,
+			expectedCode: http.StatusBadRequest,
+			expectedBody: `{"error": "bad-request-format"}`,
+			cookieIssued: false,
+		},
+		{
+			description:  "unknown error",
+			signupFunc:   func(s1, s2 string) (string, error) { return "", auth.UnknownErr },
+			body:         `{}`,
+			expectedCode: http.StatusInternalServerError,
+			expectedBody: `{"error": "unknown-error"}`,
+			cookieIssued: false,
+		},
+		{
+			description:  "normal",
+			signupFunc:   func(s1, s2 string) (string, error) { return "tokenhaha", nil },
+			body:         `{}`,
+			expectedCode: http.StatusCreated,
+			expectedBody: `{}`,
+			cookieIssued: true,
 		},
 	}
 
@@ -58,30 +98,178 @@ func TestSignupHandler(t *testing.T) {
 			authService := &MockAuthService{}
 			authService.signupFunc = tc.signupFunc
 			authHandler := handlers.NewAuthHandler(authService)
-			r := gin.New()
 
-			r.POST("/signup", authHandler.SignupHandler)
+			server := gin.New()
 
+			server.POST("/signup", authHandler.SignupHandler)
 			req := httptest.NewRequest(http.MethodPost, "/signup", bytes.NewBufferString(tc.body))
-			req.Header.Set("Content-Type", "application/json;")
+			req.Header.Set("Content-Type", "application/json")
+			res := httptest.NewRecorder()
 
-			recorder := httptest.NewRecorder()
+			server.ServeHTTP(res, req)
 
-			r.ServeHTTP(recorder, req)
+			cookies := res.Result().Cookies()
+			code := res.Code
+			body := res.Body.String()
 
-			assert.Equal(t, tc.expectedCode, recorder.Result().StatusCode)
-			assert.JSONEq(t, tc.expectedBody, recorder.Body.String())
-			assert.Contains(t, recorder.Header().Get("Content-Type"), "application/json", "Expected json type got %s", recorder.Header().Get("Content-Type"))
+			assert.Equal(t, tc.expectedCode, code, "HTTP status code mismatch")
+			assert.JSONEq(t, tc.expectedBody, body)
+
 			if tc.cookieIssued {
-				cookies := recorder.Result().Cookies()
-				require.NotEmpty(t, cookies, "Expect a cookie, found zero")
-				cookie := cookies[0]
-				assert.Equal(t, "token", cookie.Name, "Cookie name must be 'token'")
-				assert.Equal(t, "/", cookie.Path, "Cookie Must be for root /")
-
+				require.NotEmpty(t, cookies, "Expected a cookie, found none")
+				assert.Equal(t, "token", cookies[0].Name, "Cookie name must be 'token'")
+				assert.Equal(t, "/", cookies[0].Path, "Cookie path must be '/'")
 			}
 
 		})
 	}
 
+}
+
+func TestLoginHandler(t *testing.T) {
+	type LoginTestCase struct {
+		description  string
+		loginFunc    func(string, string) (string, error)
+		body         string
+		expectedCode int
+		expectedBody string
+		cookieIssued bool
+	}
+
+	testCases := []LoginTestCase{
+		{
+			description:  "Username not found",
+			loginFunc:    func(s1, s2 string) (string, error) { return "", auth.UsernameNotFoundErr },
+			body:         `{}`,
+			expectedCode: http.StatusUnauthorized,
+			expectedBody: `{"error": "invalid-credentials"}`,
+			cookieIssued: false,
+		},
+		{
+			description:  "incorrect password",
+			loginFunc:    func(s1, s2 string) (string, error) { return "", auth.IncorrectPasswordErr },
+			body:         `{}`,
+			expectedCode: http.StatusUnauthorized,
+			expectedBody: `{"error": "invalid-credentials"}`,
+			cookieIssued: false,
+		},
+		{
+			description:  "unknown error",
+			loginFunc:    func(s1, s2 string) (string, error) { return "", auth.UnknownErr },
+			body:         `{}`,
+			expectedCode: http.StatusInternalServerError,
+			expectedBody: `{"error": "unknown-error"}`,
+			cookieIssued: false,
+		},
+		{
+			description:  "non json request",
+			loginFunc:    func(s1, s2 string) (string, error) { return "", nil },
+			body:         `{`,
+			expectedCode: http.StatusBadRequest,
+			expectedBody: `{"error": "bad-request-format"}`,
+			cookieIssued: false,
+		},
+		{
+			description:  "normal",
+			loginFunc:    func(s1, s2 string) (string, error) { return "tokenhaha", nil },
+			body:         `{}`,
+			expectedCode: http.StatusOK,
+			expectedBody: `{}`,
+			cookieIssued: true,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.description, func(t *testing.T) {
+			authService := &MockAuthService{}
+			authService.loginFunc = tc.loginFunc
+			authHandler := handlers.NewAuthHandler(authService)
+
+			server := gin.New()
+
+			server.POST("/login", authHandler.LoginHandler)
+			req := httptest.NewRequest(http.MethodPost, "/login", bytes.NewBufferString(tc.body))
+			req.Header.Set("Content-Type", "application/json")
+			res := httptest.NewRecorder()
+
+			server.ServeHTTP(res, req)
+
+			cookies := res.Result().Cookies()
+			code := res.Code
+			body := res.Body.String()
+
+			assert.Equal(t, tc.expectedCode, code, "HTTP status code mismatch")
+			assert.JSONEq(t, tc.expectedBody, body)
+
+			if tc.cookieIssued {
+				require.NotEmpty(t, cookies, "Expected a cookie, found none")
+				assert.Equal(t, "token", cookies[0].Name, "Cookie name must be 'token'")
+				assert.Equal(t, "/", cookies[0].Path, "Cookie path must be '/'")
+			}
+
+		})
+	}
+
+}
+
+func TestLogoutHandler(t *testing.T) {
+	authService := &MockAuthService{}
+	authHandler := handlers.NewAuthHandler(authService)
+	server := gin.New()
+
+	server.POST("/logout", authHandler.LogoutHandler)
+	req := httptest.NewRequest(http.MethodPost, "/logout", nil)
+	res := httptest.NewRecorder()
+
+	server.ServeHTTP(res, req)
+
+	cookies := res.Result().Cookies()
+
+	require.NotEmpty(t, cookies, "Must be a cookie here")
+	assert.Equal(t, "token", cookies[0].Name, "deleted cookie name must be 'token'")
+	assert.Less(t, cookies[0].MaxAge, 0, "token age must be negative so the cookie gets deleted")
+
+	server.ServeHTTP(res, req)
+}
+
+func TestRequireAuthMiddleware(t *testing.T) {
+	authService := &MockAuthService{}
+	authHandler := handlers.NewAuthHandler(authService)
+	server := gin.New()
+	server.Use(authHandler.RequireAuthMiddleware)
+
+	// Example of not letting non logged users to play
+	server.GET("/play", func(ctx *gin.Context) {
+		ctx.Status(http.StatusOK)
+		username := ctx.GetString("username")
+		assert.Equal(t, "oussama", username, "Token returned a username that the middleware corrupted")
+	})
+
+	// Case 1, no cookie
+	req := httptest.NewRequest(http.MethodGet, "/play", nil)
+	res := httptest.NewRecorder()
+	server.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusUnauthorized, res.Code, "Status Code Mismatch")
+	assert.JSONEq(t, `{"error": "missing-token"}`, res.Body.String())
+
+	// Case 2, Cookie exist and is valid
+	authService.verifyTokenFunc = func(s string) (string, error) { return s, nil }
+	req = httptest.NewRequest(http.MethodGet, "/play", nil)
+	req.AddCookie(&http.Cookie{Name: "token", Value: "oussama"})
+	res = httptest.NewRecorder()
+	server.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusOK, res.Code)
+
+	// Case 3, Cookie exist but auth/ package says it's invalid
+	authService.verifyTokenFunc = func(s string) (string, error) { return "", auth.InvalidTokenError }
+	req = httptest.NewRequest(http.MethodGet, "/play", nil)
+	req.AddCookie(&http.Cookie{Name: "token", Value: "oussama"})
+
+	res = httptest.NewRecorder()
+	server.ServeHTTP(res, req)
+
+	assert.Equal(t, http.StatusUnauthorized, res.Code, "Status Code Mismatch")
+	assert.JSONEq(t, `{"error": "invalid-token"}`, res.Body.String())
 }
